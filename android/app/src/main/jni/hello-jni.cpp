8d61d569868cb28b4a6197ff801f83b74654e960
Joshua Reibert <joshua.reibert@gmail.com>
2015-10-12 13:45:54 +0200

    Add code trying to instantiate a class from lsd-slam

--- android/app/src/main/jni/hello-jni.cpp
+++ android/app/src/main/jni/hello-jni.cpp
@@ -1,13 +1,14 @@
 
-#include <string.h>
 #include <jni.h>
-#include <lsdtest.hpp>
-#include <string>
+#include <lsd_slam/util/EigenCoreInclude.h>
+#include <lsd_slam/DataStructures/Frame.h>
 
 extern "C" {
 jstring
 Java_de_joshuareibert_dontslamyourhead_MainActivity_stringFromJNI(JNIEnv* env,
                                                  jobject thiz) {
-    return env->NewStringUTF(helloFromLsdSlam().c_str());
+    unsigned char* img = new unsigned char();
+    lsd_slam::Frame frame(0, 320, 240, Eigen::Matrix3f(), 0, img);
+    return env->NewStringUTF("Yay! It worked.");
 }
 }
