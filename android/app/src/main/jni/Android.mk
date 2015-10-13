c846f151fd1519256d4242c61d13e3c064318a1b
Joshua Reibert <joshua.reibert@gmail.com>
2015-10-13 10:12:17 +0200

    Adapt to new lib-folder

--- android/app/src/main/jni/Android.mk
+++ android/app/src/main/jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := lsd-slam
-LOCAL_SRC_FILES := liblsd-slam.so
+LOCAL_SRC_FILES := lib/liblsd-slam.so
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 include $(PREBUILT_SHARED_LIBRARY)
 
