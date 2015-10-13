3f7ce56b8fab29d26f16ae27e697fc5d7cdee2aa
Joshua Reibert <joshua.reibert@gmail.com>
2015-10-13 10:49:31 +0200

    Remove comments from original ndksample

--- android/app/src/main/jni/hello-jni.cpp
+++ android/app/src/main/jni/hello-jni.cpp
@@ -1,37 +1,14 @@
-/*
- * Copyright (C) 2009 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+
 #include <string.h>
 #include <jni.h>
 #include "lsd-slam/test.hpp"
 #include <string>
 
-/* This is a trivial JNI example where we use a native method
- * to return a new VM String. See the corresponding Java source
- * file located at:
- *
- *   apps/samples/hello-jni/project/src/com/example/hellojni/HelloJni.java
- */
 extern "C" {
 jstring
 Java_de_joshuareibert_dontslamyourhead_MainActivity_stringFromJNI(JNIEnv* env,
                                                  jobject thiz) {
-
     std::string test = helloFromLSDSLAM();
-
     return env->NewStringUTF(test.c_str());
 }
 }
