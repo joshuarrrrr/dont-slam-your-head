9d41c89b5643046648db73eae59df42d182232f5
Joshua Reibert <joshua.reibert@gmail.com>
2015-10-16 14:41:49 +0200

    Instantiate class from lsd-slam and testprint a value

--- android/app/src/main/jni/hello-jni.cpp
+++ android/app/src/main/jni/hello-jni.cpp
@@ -1,14 +1,24 @@
 
 #include <string.h>
 #include <jni.h>
-#include "lsd-slam/test.hpp"
+#include "lsd_slam/DataStructures/Frame.h"
+#include <Eigen/Dense>
 #include <string>
+#include <sstream>
+
+std::string const doubleToString(double d) {
+    std::ostringstream ostr;
+    ostr << d;
+    return ostr.str();
+}
 
 extern "C" {
 jstring
 Java_de_joshuareibert_dontslamyourhead_MainActivity_stringFromJNI(JNIEnv* env,
                                                  jobject thiz) {
-    std::string test = helloFromLSDSLAM();
-    return env->NewStringUTF(test.c_str());
+    const unsigned char* image = new unsigned char();
+    lsd_slam::Frame frame(0, 320, 240, Eigen::Matrix3f(), 27.5, image);
+    std::string txt = "Yay! It works and returns '" + doubleToString(frame.timestamp()) + "'";
+    return env->NewStringUTF(txt.c_str());
 }
 }
