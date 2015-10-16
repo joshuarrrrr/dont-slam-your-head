8f75b452f40e7982e1ba8607b1bd126d7d0fdc06
Joshua Reibert <joshua.reibert@gmail.com>
2015-10-16 15:28:32 +0200

    Unify name to lsd_slam

--- android/app/src/main/jni/Android.mk
+++ android/app/src/main/jni/Android.mk
@@ -62,21 +62,19 @@ include $(PREBUILT_STATIC_LIBRARY)
 
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := lsd-slam
-LOCAL_SRC_FILES := lib/liblsdslam.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-LOCAL_SHARED_LIBRARIES := g2o_core g2o_solver_csparse
+LOCAL_MODULE := lsd_slam
+LOCAL_SRC_FILES := lib/liblsd_slam.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := hello-jni
 LOCAL_SRC_FILES := hello-jni.cpp
-LOCAL_CFLAGS += -std=c++11
+LOCAL_CPPFLAGS += -std=c++11
 LOCAL_C_INCLUDES += /usr/include/eigen3
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/lsd_slam
 LOCAL_C_INCLUDES += /home/joshua/Android/android-ndk-r10d/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/user/include
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/boost/include/
 LOCAL_STATIC_LIBRARIES := boost_system boost_thread
-LOCAL_SHARED_LIBRARIES := lsd-slam
+LOCAL_SHARED_LIBRARIES := lsd_slam
 include $(BUILD_SHARED_LIBRARY)
