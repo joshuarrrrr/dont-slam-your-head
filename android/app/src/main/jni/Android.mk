a7bcb84840ab200437e5d536f5fde34354719519
Joshua Reibert <joshua.reibert@gmail.com>
2015-10-22 14:25:56 +0200

    Add opencv

--- android/app/src/main/jni/Android.mk
+++ android/app/src/main/jni/Android.mk
@@ -12,6 +12,9 @@ LOCAL_SRC_FILES := lib/liblsd_slam.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+OPENCV_INSTALL_MODULES := on
+OPENCV_CAMERA_MODULES := on
+include /home/joshua/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
 LOCAL_MODULE    := lsd-jni
 LOCAL_SRC_FILES := lsd-jni.cpp
 LOCAL_CPPFLAGS += -std=c++11
@@ -20,5 +23,5 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/lsd_slam
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/g2o/include
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/boost/include/
 LOCAL_STATIC_LIBRARIES := boost_system boost_thread
-LOCAL_SHARED_LIBRARIES := lsd_slam
+LOCAL_SHARED_LIBRARIES := lsd_slam opencv_java3
 include $(BUILD_SHARED_LIBRARY)
