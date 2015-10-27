870c2b240321d3db4dd1848206399568c2aa52a0
Joshua Reibert <joshua.reibert@gmail.com>
2015-10-27 10:27:57 +0100

    Add line to check if LiveSLAMWrapper can be instantiated

--- android/app/src/main/jni/lsd-jni.cpp
+++ android/app/src/main/jni/lsd-jni.cpp
@@ -1,6 +1,7 @@
 
 #include <string.h>
 #include <jni.h>
+#include <lsd_slam/LiveSLAMWrapper.h>
 #include <lsd_slam/DataStructures/Frame.h>
 #include <Eigen/Dense>
 #include <string>
@@ -18,9 +19,12 @@ extern "C" {
 jstring
 Java_de_joshuareibert_dontslamyourhead_MainActivity_stringFromJNI(JNIEnv* env,
                                                  jobject thiz) {
+    lsd_slam::LiveSLAMWrapper slam(nullptr, nullptr);
+    std::string txt = "Yay! It works.";
+
     const unsigned char* image = new unsigned char();
     lsd_slam::Frame frame(0, 320, 240, Eigen::Matrix3f(), 27.5, image);
-    std::string txt = "Yay! It works and returns '" + doubleToString(frame.timestamp()) + "'";
+     txt += "\nAnd the frame returns '" + doubleToString(frame.timestamp()) + "'";
 
     cv::Mat mat = cv::Mat(1, 1, CV_64F, 3.5);
     txt += "\nAnd there is even a cv::Mat with the value '" + doubleToString(mat.at<double>(0,0))
