0193bbbc110c714121ce329bcf5e82e41159a2eb
Joshua Reibert <joshua.reibert@gmail.com>
2016-01-12 18:24:31 +0100

    Add tracking script that updates camera position with tracking data

---
+++ unity/Assets/Tracking.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tracking : MonoBehaviour {
+
+	private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		Transform transform = GetComponent<Transform>();
+		startPosition = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		AndroidJavaObject activityObj = MainActivity.activityObj;
+		Vector3 trackingPosition = new Vector3(
+			activityObj.Call<float>("getTranslationX"),
+			activityObj.Call<float>("getTranslationY"),
+			activityObj.Call<float>("getTranslationZ")
+			);
+		transform.position = startPosition + trackingPosition;
+		Debug.Log(transform.position);
+	}
+}
