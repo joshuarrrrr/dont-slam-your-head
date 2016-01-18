45a3d11a714b014a10942f1f2d59f9ac8d5aa8fe
Joshua Reibert <joshua.reibert@gmail.com>
2016-01-18 14:15:39 +0100

    Add verbose flag

--- utils/update-unity.sh
+++ utils/update-unity.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -u ./android/app/build/intermediates/bundles/armv7/debug/classes.jar ./unity/Assets/Plugins/Android/
-cp -u ./android/app/src/main/libs/armeabi-v7a/*.so ./unity/Assets/Plugins/Android/libs/armeabi-v7a/
-cp -u ./android/libraries/opencv/build/intermediates/bundles/release/classes.jar ./unity/Assets/Plugins/Android/OpenCV.jar
+cp -uv ./android/app/build/intermediates/bundles/armv7/debug/classes.jar ./unity/Assets/Plugins/Android/
+cp -uv ./android/app/src/main/libs/armeabi-v7a/*.so ./unity/Assets/Plugins/Android/libs/armeabi-v7a/
+cp -uv ./android/libraries/opencv/build/intermediates/bundles/release/classes.jar ./unity/Assets/Plugins/Android/OpenCV.jar
 
