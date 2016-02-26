cfb49882beea655fce2157a78741441ee16714d4
Joshua Reibert <joshua.reibert@gmail.com>
2016-02-26 17:14:17 +0100

    Use initial rotation from cardboard sdk

--- unity/Assets/BoxCloud.cs
+++ unity/Assets/BoxCloud.cs
@@ -5,25 +5,38 @@ public class BoxCloud : MonoBehaviour {
 
 	public int num = 500;
 
-	private GameObject box = null;
-
 	private GameObject[] cloud;
 
-	void Start () {
-		Vector3  scale = Vector3.one * 0.02F;
-		box = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		box.GetComponent<Transform>().localScale = scale;
+	public Vector3 boxSize = Vector3.one * 0.075F;
 
-		cloud = new GameObject[num];
-		for (int i = 0; i < num; ++i) {
-			cloud[i] = Instantiate(box);
-			// cloud[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-			cloud[i].GetComponent<Renderer>().enabled = false;
-		}
+	// the unknown scale of the whole point cloud
+	public float cloudScale = 5.0F;
+
+	private bool initialRotSet = false;
+	private Quaternion initialRot;
+
+	void Start () {
+		initCloud();
 	}
 
 	void Update() {
+		if (!initialRotSet) {
+			var rot = Cardboard.SDK.HeadPose.Orientation;
+			Debug.Log("initial rotation: " + Quaternion.identity);
+			if (rot != Quaternion.identity) {
+				initialRot = rot;
+				initialRotSet = true;
+				Debug.Log("initial rotation: " +
+					rot.eulerAngles.x.ToString() + " deg x, " +
+					rot.eulerAngles.y.ToString() + " deg y, " +
+					rot.eulerAngles.z.ToString() + " deg z"
+					);
+			}
+		}
+
 		float[] points = MainActivity.activityObj.Call<float[]>("getPoints");
+
+		Vector3 offset = GetComponent<Transform>().position;
 
 		for (int i = 0; i < num; ++i) {
 			float x = points[i * 3];
@@ -32,11 +45,29 @@ public class BoxCloud : MonoBehaviour {
 			if ((x == 0.0F) && (y == 0.0F) && (z == 0.0F)) {
 				cloud[i].GetComponent<Renderer>().enabled = false;
 			} else {
-				Vector3 pos = new Vector3(x, y, z) +
-					GetComponent<Transform>().position;
-				cloud[i].GetComponent<Transform>().position = pos;
+				Vector3 pos = new Vector3(x, y, z) * cloudScale;
+				pos = initialRot * pos;
+				cloud[i].GetComponent<Transform>().position = pos + offset;
 				cloud[i].GetComponent<Renderer>().enabled = true;
 			}
 		}
+	}
+
+	private void initCloud() {
+		cloud = new GameObject[num];
+		for (int i = 0; i < num; ++i) {
+			cloud[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			cloud[i].GetComponent<Transform>().localScale = boxSize;
+			cloud[i].GetComponent<Renderer>().enabled = false;
+		}
+	}
+
+	public void Reset() {
+		initialRotSet = false;
+		initCloud();
+	}
+
+	public void SetScale(float scale) {
+		cloudScale = scale;
 	}
 }
